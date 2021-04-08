cfa3959133f740dfcb5bfe273678ef9529bb5265
n1ckDotEXE <admin@nicholasnam.com>
2021-04-08 15:10:40 -0400

    Users schema setup in backend

---
+++ passport-auth-back/routes/users/model/User.js
@@ -0,0 +1,23 @@
+mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		trim: true,
+		unique: true,
+	},
+	email: {
+		type: String,
+		trim: true,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	userCreated: {
+		type: Date,
+		default: new Date(),
+	},
+});
+module.export = mongoose.model("user", UserSchema);
